fe8dd7e49fde58c7d77e4c984713d4d78f873324
Vildan Tursic <vildantursic@hotmail.com>
2016-04-06 22:32:15 +0200

    added heroes array to get reuqest for angular2 app

--- app.ts
+++ app.ts
@@ -18,6 +18,12 @@ const bruteforce = new expressBrute(store);
 // importing API
 import {apiRoute} from "./routes/api-route";
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
--- routes/api-route.ts
+++ routes/api-route.ts
@@ -9,10 +9,21 @@ const api = router.route("/api/:entity");
 api.get((req: Request, res: Response) => {
 
   let entity: string = req.params.entity;
-  res.json({
-    entity: entity,
-    method: "get"
-  });
+
+  let heroes = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco 2" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+  ];
+
+  res.json(heroes);
 
 });
 api.post((req: Request, res: Response) => {
