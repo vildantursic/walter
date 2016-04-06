5068804ac33353d30f17ff3b3f326948902f8927
Vildan Tursic <vildantursic@hotmail.com>
2016-04-06 16:56:05 +0200

    body-parser added, and reconfigured package.json file

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
 /// <reference path="typings/main.d.ts" />
 
 import * as express from "express";
+import * as bodyParser from "body-parser";
 const app: express.Application = express();
 
 // importing API
 import {apiRoute} from "./app/apiRoute";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/", apiRoute);
 
--- app/apiRoute.ts
+++ app/apiRoute.ts
@@ -18,20 +18,18 @@ api.get((req: Request, res: Response) => {
 api.post((req: Request, res: Response) => {
 
   let entity: string = req.params.entity;
-  res.send({
-    entity: entity,
-    method: "post"
-  });
+
+  let reqObject: Object = req.body;
+  res.json(reqObject);
 
 });
 
 api.put((req: Request, res: Response) => {
 
   let entity: string = req.params.entity;
-  res.send({
-    entity: entity,
-    method: "put"
-  });
+
+  let reqObject: Object = req.body;
+  res.json(reqObject);
 
 });
 api.delete((req: Request, res: Response) => {
