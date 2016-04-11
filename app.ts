86dd245923e0ccc939393f5cfc900de765b1807e
Vildan Tursic <vildantursic@hotmail.com>
2016-04-11 11:38:00 +0200

    removed angular app

--- app.ts
+++ app.ts
@@ -2,21 +2,21 @@
 
 import * as express from "express";
 import {urlencoded, json} from "body-parser";
-import * as expressBrute from "express-brute";
+// import * as expressBrute from "express-brute";
 const app: express.Application = express();
 
 app.use(express.static("./public"));
 app.use(express.static("./public/node_modules"));
 
-let bruteForceConf = {
-  freeRetries: 5,
-  proxyDepth: 1,
-  minWait: 5 * 60 * 1000,
-  maxWait: 60 * 60 * 1000
-};
+// let bruteForceConf = {
+//   freeRetries: 5,
+//   proxyDepth: 1,
+//   minWait: 5 * 60 * 1000,
+//   maxWait: 60 * 60 * 1000
+// };
 
-const store = new expressBrute.MemoryStore(); // not for production
-const bruteforce = new expressBrute(store);
+// const store = new expressBrute.MemoryStore(); // not for production
+// const bruteforce = new expressBrute(store);
 
 // importing API
 import {apiRoute} from "./routes/api-route";
