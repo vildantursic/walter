61c4c6d5e2f2f470f438c1580c86635dc4a2aafe
Vildan Tursic <vildantursic@hotmail.com>
2016-04-13 12:24:20 +0200

    checking data validity and making separate function for exec, not sure if that is right, trying to make DRY code

--- routes/complex.ts
+++ routes/complex.ts
@@ -4,49 +4,37 @@ import {Request, Response, Router} from "express";
 const router: Router = Router();
 import {complexModel} from "../models/models";
 
-let checkData: Function = (param: Object): Boolean => {
-    if (param) {
+let checkData: Function = (param: String): Boolean => {
+    // add more filters to data to see if it fit your needs
+    if (param.length === 24) {
         return true;
     }
     return false;
 };
 
+let returnObject: Function = (err: Error, data: Object) => {
+    if (err) {
+        throw err;
+    }
+    return data;
+};
+
 const api = router.route("/api/complex/:id*?");
 
-api.get((req: Request, res: Response) => {
-
-    let check = checkData(req.params.id);
-
-    complexModel.find(check, (err: Error, data: Array<Object>) => {
-        if (err) {
-            throw err;
-        }
-        res.json(data);
-    });
-    // await complexModel.find({_id: id}).exec((err: Error, data) => {
-    //     if (err) {
-    //         throw err;
-    //     }
-    //     res.json(data);
-    // });
+api.get(async (req: Request, res: Response) => {
+    if (!checkData(req.params.id)) {
+        res.json("invalid object for search");
+    } else {
+        let obj: Object = await complexModel.find({_id: req.params.id}).exec(returnObject);
+        res.json(obj);
+    }
 });
 
 api.post((req: Request, res: Response) => {
 
-    let data: Object = req.body;
-    let complex = new complexModel(data);
-    complex.save((err: Error) => {
-        if (err) {
-            throw err;
-        }
-        res.json("Document is saved");
-    });
-});
-
-api.put((req: Request, res: Response) => {
-
-    let id: string = req.params.id;
-    complexModel.update({ _id: id }, { $set: req.body }, (err: Error, data: Array<Object>) => {
+    // check data before saving
+    let obj = new complexModel(req.body);
+    obj.save((err: Error, data: Object) => {
         if (err) {
             throw err;
         }
@@ -54,15 +42,18 @@ api.put((req: Request, res: Response) => {
     });
 });
 
-api.delete((req: Request, res: Response) => {
+api.put(async (req: Request, res: Response) => {
 
     let id: string = req.params.id;
-    complexModel.findByIdAndRemove(id, (err: Error, data: Array<Object>) => {
-        if (err) {
-            throw err;
-        }
-        res.json(data);
-    });
+    let obj: Object = await complexModel.update({ _id: id }, { $set: req.body }).exec(returnObject);
+    res.json(obj);
+});
+
+api.delete(async (req: Request, res: Response) => {
+
+    let id: string = req.params.id;
+    let obj: Object = await complexModel.findByIdAndRemove(id).exec(returnObject);
+    res.json(obj);
 });
 
 export const apiComplex = router;
