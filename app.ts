1476fc5b1d96940d3977f9885f0769fb8c149685
Vildan Tursic <vildantursic@hotmail.com>
2016-04-13 14:09:37 +0200

    brute configured and working

--- app.ts
+++ app.ts
@@ -2,26 +2,30 @@
 
 import * as express from "express";
 import {urlencoded, json} from "body-parser";
-import * as expressBrute from "express-brute";
+import * as ExpressBrute from "express-brute";
 const app: express.Application = express();
+import {Server} from "http";
 
 // importing API
 import {apiComplex} from "./routes/complex";
 import {apiEntity} from "./routes/entity";
 import {apiObject} from "./routes/object";
 
-const bruteForce = new expressBrute({
-   freeRetries: 10
+var store = new ExpressBrute.MemoryStore(); // stores state locally, don't use this in production
+var bruteForce = new ExpressBrute(store, {
+   freeRetries: 10,
+   minWait: 60 * 1000
 });
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
-app.use("/", apiComplex);
-app.use("/", apiEntity);
-app.use("/", apiObject);
+// app.use("/", bruteForce.prevent);
+app.use("/", bruteForce.prevent, apiComplex);
+app.use("/", bruteForce.prevent, apiEntity);
+app.use("/", bruteForce.prevent, apiObject);
 
-const server = app.listen(4000, "localhost", () => {
+const server: Server = app.listen(4000, "localhost", () => {
    const port: number = server.address().port;
    console.log("Listening on http://localhost:" + port);
 });
